d0ec58ad793243aaabba07bdf16eea901a80064d
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-02-10 19:00:17 +0630

    adding NewItemForm component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import {
-  AppContainer,
-  ColumnContainer,
-  ColumnTitle,
-  CardContainer,
+  AppContainer
 } from "./styles";
 import Column from "./Column";
+import { AddNewItem } from "./AddNewItem";
 
 const App = () => {
   return (
     <AppContainer>
       <Column text="Todo:" />
+      <AddNewItem onAdd={console.log} toggleButtonText={`Add another list`} />
     </AppContainer>
   );
 };

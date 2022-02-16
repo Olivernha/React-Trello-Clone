1355fd85f2c517e9fba139bff1db004b7c3160dd
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-02-16 22:21:05 +0630

    Drag Cards

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,42 +1,42 @@
-import { useRef } from "react"
-import { ColumnContainer, ColumnTitle } from "./styles"
-import { useAppState } from "./state/AppStateContext"
-import { Card } from "./Card"
-import { AddNewItem } from "./AddNewItem"
-import { useItemDrag } from "./utils/useItemDrag"
-import { useDrop } from "react-dnd"
-import { isHidden } from "./utils/isHidden"
-import { moveList, addTask } from "./state/actions"
+import { useRef } from "react";
+import { ColumnContainer, ColumnTitle } from "./styles";
+import { useAppState } from "./state/AppStateContext";
+import { Card } from "./Card";
+import { AddNewItem } from "./AddNewItem";
+import { useItemDrag } from "./utils/useItemDrag";
+import { useDrop } from "react-dnd";
+import { isHidden } from "./utils/isHidden";
+import { moveList, addTask } from "./state/actions";
 
 type ColumnProps = {
-  text: string
-  id: string
-  isPreview?: boolean
-}
+  text: string;
+  id: string;
+  isPreview?: boolean;
+};
 
 export const Column = ({ text, id, isPreview }: ColumnProps) => {
-  const { draggedItem, getTasksByListId, dispatch } = useAppState()
-  const tasks = getTasksByListId(id)
-  const ref = useRef<HTMLDivElement>(null)
+  const { draggedItem, getTasksByListId, dispatch } = useAppState();
+  const tasks = getTasksByListId(id);
+  const ref = useRef<HTMLDivElement>(null);
   const [, drop] = useDrop({
     accept: "COLUMN",
     hover() {
       if (!draggedItem) {
-        return
+        return;
       }
       if (draggedItem.type === "COLUMN") {
         if (draggedItem.id === id) {
-          return
+          return;
         }
 
-        dispatch(moveList(draggedItem.id, id))
+        dispatch(moveList(draggedItem.id, id));
       }
-    }
-  })
+    },
+  });
 
-  const { drag } = useItemDrag({ type: "COLUMN", id, text })
+  const { drag } = useItemDrag({ type: "COLUMN", id, text });
 
-  drag(drop(ref))
+  drag(drop(ref));
 
   return (
     <ColumnContainer
@@ -46,7 +46,7 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
     >
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
-        <Card text={task.text} key={task.id} />
+        <Card columnId={id} text={task.text} key={task.id} id={task.id} />
       ))}
       <AddNewItem
         toggleButtonText="+ Add another card"
@@ -54,5 +54,5 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
         dark
       />
     </ColumnContainer>
-  )
-}
+  );
+};
